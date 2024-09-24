08e1777eb7d39953960e1dcbc95c860a45337d2b
lbs-joro <johan.ronnas@ga.lbs.se>
2024-09-24 12:45:01 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace FirstProgramSu24b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int diceNumber = rand.Next(1, 7);
+
+            Console.WriteLine(diceNumber);
+
+
+
+        }
+    }
+}
